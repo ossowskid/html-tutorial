c69f77cd0ab0a488c63b7c15cdb7c3cc9df77562
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-03-06 18:29:38 +0100

    lesson 10 javascript by netninja

--- javascript-tutorial/script.js
+++ javascript-tutorial/script.js
@@ -1,1 +1,12 @@
-alert("hello world");
+const myVar = "Dawid"
+
+document.write(myVar);
+console.log(myVar);
+
+document.write(10);
+
+console.log(50);
+
+console.log(20 * 5);
+
+document.write(20 / 5);
