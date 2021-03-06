18d9c82494140de421f5ebfa4f581b5589d3b3b5
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-03-06 20:11:45 +0100

    lesson 20 javascript tutorial by NetNinja

--- javascript-tutorial/script.js
+++ javascript-tutorial/script.js
@@ -1,7 +1,11 @@
-const links = document.getElementsByTagName("a");
-
-for (i = 0; i < links.length; i++) {
-
-    links[i].className = "link-" + i;
-
+function getAverage(a, b,c,d,e,f) {
+    
+    const average = (a + b + c + d + e + f) / 6;
+    console.log(average);
+    return average;
 }
+
+
+const myResult = getAverage(7, 8, 9, 10, 11, 12);
+
+console.log("the average is " + myResult);
