100c9b18fb0062377fef895abd07c7e3c69efc8a
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-03-07 17:38:38 +0100

    javascript tutorial by NetNinja lesson 22

--- javascript-tutorial/script.js
+++ javascript-tutorial/script.js
@@ -1,6 +1,10 @@
-const a = 5;
-const b = 6;
+const a = "hello";
+const b = 5;
 
-console.log(a + b);
+if (!isNaN(a)) {
+    console.log("meaning of life is " + (a * b));
+} else {
+    
+    console.log("that aint even a number, thickie")
+}
 
-console.log(Math.PI);
