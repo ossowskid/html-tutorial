c000c31e6eff08e7302cfb32543fe331b18064a2
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-03-07 17:25:37 +0100

    lesson 21 javascript tutorial by NetNinja

--- javascript-tutorial/script.js
+++ javascript-tutorial/script.js
@@ -1,11 +1,19 @@
-function getAverage(a, b,c,d,e,f) {
+let average = 0;
+
+function getAverage(a, b) {
     
-    const average = (a + b + c + d + e + f) / 6;
+    average = (a + b) / 2;
     console.log(average);
     return average;
 }
 
 
-const myResult = getAverage(7, 8, 9, 10, 11, 12);
+const myResult = getAverage(7, 11);
 
 console.log("the average is " + myResult);
+
+
+function logResult() {
+    console.log("the average is " + myResult + " inside the function");
+}
+logResult();
