b3ace738dbf2d3b7e5eb5c262f7a008c134528c5
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-03-07 17:57:19 +0100

    javascript lesson 25 by NetNinja tutorial

--- javascript-tutorial/script.js
+++ javascript-tutorial/script.js
@@ -1,4 +1,19 @@
-const string1 = "Abc";
-const string2 = "Bcd";
+const str = "hello, world";
 
-console.log(string1 < string2);
+console.log(str);
+
+const str2 = str.slice(2, 9);
+
+console.log(str2);
+
+const str3 = str.slice(2);
+
+console.log(str3);
+
+const tags = "meat, ham, salami, pork, beef, chicken";
+
+console.log(tags);
+
+const tagsArray = tags.split(",");
+
+console.log(tagsArray);
