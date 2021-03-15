0bc826aa1e693a93695b2df9922c053487a24107
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-03-15 19:28:35 +0100

    lesson 30 javascript tutorial by NetNinja

--- javascript-tutorial/script.js
+++ javascript-tutorial/script.js
@@ -1,17 +1,33 @@
-const myCar2 = {
-    maxSpeed: 70,
-    driver: "David",
-    drive: function (speed, time) {
-        console.log(speed * time);
-    },
+// const myCar = {
+//     maxSpeed: 70,
+//     driver: "David",
+//     drive: function (speed, time) {
+//         console.log(speed * time);
+//     },
 
-    logDriver: function () {
+//     logDriver: function () {
+//         console.log("Driver name is " + this.driver);
+//     }
+// };
+
+const Car = function (maxSpeed, driver) {
+    
+    this.maxSpeed = maxSpeed;
+    this.driver = driver;
+    this.drive = function (speed, time) {
+        console.log(speed * time);
+    };
+    this.logDriver = function () {
         console.log("Driver name is " + this.driver);
     }
-}
+
+};
 
 
+const myCar = new Car(70, "Ninja Man");
+const myCar2 = new Car(10, "Humpty Dumpty");
+const myCar3 = new Car(20, "Dawid");
+const myCar4 = new Car(900, "James Bond");
 
-myCar2.logDriver();
-console.log(myCar2.maxSpeed);
-myCar2.drive(50, 3);
+myCar.drive(30, 5);
+myCar3.logDriver();
