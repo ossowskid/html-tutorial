611ecf1921e5df16ba67aed430952481638fc83b
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-03-15 18:57:48 +0100

    lesson 27 javascript tutorial by NetNinja

--- javascript-tutorial/script.js
+++ javascript-tutorial/script.js
@@ -1,28 +1,9 @@
-const myArray = [];
+let myString = new String();
 
-myArray[0] = 25;
-myArray[1] = 35;
-myArray[2] = true;
-myArray[3] = "hello";
-myArray[2] = false;
+myString = "hello";
 
-console.log(myArray);
+console.log(myString.length);
 
-const myArray2 = [10, 20, "hi", false];
+let myString2 = "hello there Kenobi";
 
-console.log(myArray2);
-
-const myArray3 = new Array();
-
-
-console.log(myArray3);
-
-const myArray4 = new Array(5);
-
-console.log(myArray4);
-
-console.log(myArray2.length);
-
-console.log(myArray2.sort());
-
-console.log(myArray2.reverse());
+console.log(myString2);
