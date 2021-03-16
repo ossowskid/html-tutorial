f61bc4e4fd57e2790809bd871fcfa644066d1928
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-03-16 20:00:28 +0100

    lesson 41 javascript tutorial by NetNinja

--- javascript-tutorial/script.js
+++ javascript-tutorial/script.js
@@ -1,14 +1,21 @@
-const content = document.getElementById("content");
-const button = document.getElementById("show-more");
+function setUpEvents() {
+    const content = document.getElementById("content");
+    const button = document.getElementById("show-more");
 
-button.onclick = function () {
-    if (content.className === "open") {
-      //shrink the box
-        content.className = "";
-        button.innerHTML = "Show More";
-    } else {
-      //expand the box
-        content.className = "open";
-        button.innerHTML = "Show Less";
-  }
+    button.onclick = function () {
+        if (content.className === "open") {
+        //shrink the box
+            content.className = "";
+            button.innerHTML = "Show More";
+        } else {
+        //expand the box
+            content.className = "open";
+            button.innerHTML = "Show Less";
+    }
+    };
+};
+
+
+window.onload = function () {
+    setUpEvents();
 };
