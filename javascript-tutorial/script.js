112c13e2051c7852eabf6ea62d5174705ef17e9a
Dawid Ossowski <ossowski.dawid@gmail.com>
2021-03-17 19:03:57 +0100

    lesson 44 javascript tutorial by NetNinja

--- javascript-tutorial/script.js
+++ javascript-tutorial/script.js
@@ -1,27 +1,12 @@
-// const myMessage = document.getElementById("message");
+const myForm = document.forms.myForm;
+const message = document.getElementById("message");
 
-// function showMessage() {
-//     myMessage.className = "show";
-// }
-
-// setTimeout(showMessage, 3000);
-
-let colourChanger = document.getElementById("colour-changer");
-let colours = ["#ff9999", "#ggggg", "#586432", "#234543"];
-let counter = 0;
-
-function changeColour() {
-    if (counter >= colours.length) {
-        counter = 0;
-}
-
-    colourChanger.style.background = colours[counter];
-    counter++;
-}
-
-let myTimer = setInterval(changeColour, 1000);
-
-colourChanger.onclick = function () {
-    clearInterval(myTimer);
-    colourChanger.innerHTML = "Timer stopped";
-}
+myForm.onsubmit = function () {
+    if (myForm.name.value == "") {
+        message.innerHTML = "please enter a name";
+        return false;
+    } else {
+        message.innerHTML = "";
+        return true;
+  }
+};
